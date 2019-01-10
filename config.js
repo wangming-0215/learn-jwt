6c2643f35a8f7fbca47dff26f204e3f5b1734f38
wangming <ming.wang@buttoninvest.com>
2019-01-10 15:34:22 +0800

    WIP

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ module.exports = {
   port: 3001,
   secret: 'tangdaiqing',
   dbURL: 'mongodb://localhost/wangming-jwt',
-  // expiresIn: 86400000
-  expiresIn: 2 * 60 * 1000 // 2min
+  expiresIn: 86400000
+  // expiresIn: 2 * 60 * 1000 // 2min
 };
